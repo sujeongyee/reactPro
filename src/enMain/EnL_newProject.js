be6ed2aa9a544ff4b515d85ea4f868cfc242717c
sol0916 <yj0916lee00@gmail.com>
2023-09-25 12:19:05 +0900

    230925 예지

--- src/enMain/EnL_newProject.js
+++ src/enMain/EnL_newProject.js
@@ -6,10 +6,16 @@ import "../enMain/EnCss.css";
 import FormControlIcon from "../img/FormControlIcon";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Pagination from "react-js-pagination";
+import SearchIcon from "../engineerLeader/SearchIcon";
 
 function EnL_newProject({ checkPermission }) {
   const eng_enid = checkPermission.sub;
   const [loading, setLoading] = useState(true);
+  const [first, setFirst] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 정보 저장
+  const itemsPerPage = 10; // 페이지당 아이템 수
+
 
   const [data, setData] = useState([]);
   console.log(data);
@@ -19,13 +25,54 @@ function EnL_newProject({ checkPermission }) {
       .get(`/api/main/engineer/newList/${eng_enid}`)
       .then((response) => {
         setData(response.data);
-
-        setLoading(false);
+        setFirst(response.data);
+        // setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  const handlePageChange = (page) => { // 페이지 핸들링
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (e) => { //search 버튼을 눌렀을때의 이벤트
+
+    const searchWord = document.querySelector(".select-word-engl").value; //검색 단어
+
+    const filter = document.querySelector(".selectee").value; // 회사명 프로젝트명(옵션값)
+////////////////////////////////////////////////
+    // 데이터를 복사하여 필터링
+    const filteredList = first.filter((item) => { //검색시작
+      if (filter === "프로젝트명") {
+        return item.pro_name.includes(searchWord);
+      } else if (filter === "회사명") {
+        return item.cus_company_name.includes(searchWord);
+      } else if (filter === "계약상태") {
+        return item.pro_status.includes(searchWord);
+      } else if (filter === "전체" && searchWord === "") {
+        return item;
+      } else if (filter === "전체") {
+        return (
+          item.pro_name.includes(searchWord) || item.cus_company_name.includes(searchWord) || item.pro_status.includes(searchWord)
+        );
+      }
+      return true;
+  });
+
+  // 필터링된 데이터를 업데이트
+  setData(filteredList);
+  setCurrentPage(1); // 페이지를 첫 번째 페이지로 리셋
+}; // 검색끝
+
+  const indexOfLastItem = currentPage * itemsPerPage; //마지막 페이지 계산
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage; 
+  var currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+
+
+
 
   return (
     <>
@@ -44,7 +91,7 @@ function EnL_newProject({ checkPermission }) {
           <div className="row">
             <div className="col-12">
               <div className="card1">
-                <div className="customize-input float-end search-btn">
+                {/* <div className="customize-input float-end search-btn">
                   <Link className="nav-link" href="javascript:void(0)">
                     <form className="search-engineer">
                       <div className="customize-input right">
@@ -61,8 +108,34 @@ function EnL_newProject({ checkPermission }) {
                       <div></div>
                     </form>
                   </Link>
-                </div>
+                </div> */}
                 <div className="card-body1">
+                <form className="search-engineer search-englg" style={{position:'absolute', top:'0px', right: '100px', margin: '0 5px', marginBottom:'20px' }}>
+                  <div className="customize-input right select-proengl">
+
+                    <select style={{ display: 'inline-block' }} className="selectee">
+                      <option className="selecteeop">전체</option>
+                      <option className="selecteeop">프로젝트명</option>
+                      <option className="selecteeop">회사명</option>
+                      <option className="selecteeop">계약상태</option>
+                    </select>
+                  </div>
+
+                  <div className="customize-input right" style={{ marginLeft: '10px' }}>
+
+                    <input
+                      className="form-control custom-shadow custom-radius border-0 bg-white select-word-engl"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                  </div>
+                  <div className="customize-input left search-click-eng" style={{ marginLeft: '10px', marginRight: '5px' }} onClick={handleSearch}> 
+                    <SearchIcon color="#9cbba6" />
+                  </div>
+                </form>
+
+
                   <div className="table-responsive">
                     <div className="project-table">
                       <table className="table">
@@ -79,29 +152,15 @@ function EnL_newProject({ checkPermission }) {
                         </thead>
 
                         <tbody>
-                          {data.map((project, index) => (
+                          {currentItems.map((project, index) => (
                             <tr key={project.pro_id}>
-                              <td>{index + 1}</td>
+                              <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
                               <td>
                                 <Link
                                   to={{
                                     pathname: `/engineer/newProjectDetail/${project.pro_id}`,
                                   }}
                                   state={{ project }}
-
-                                  // state={{
-                                  //   cus_company_name: project.cus_company_name,
-                                  //   cus_boss: project.cus_boss,
-                                  //   cus_company_ph: project.cus_company_ph,
-                                  //   cus_business_id: project.cus_business_id,
-                                  //   cus_address1: project.cus_address1,
-                                  //   cus_address2: project.cus_address2,
-                                  //   pro_startDate: project.pro_startDate,
-                                  //   pro_endDate: project.pro_endDate,
-                                  //   pro_rep: project.pro_rep,
-                                  //   cus_phone_number: project.cus_phone_number,
-                                  //   cus_email: project.cus_email,
-                                  //}}
                                 >
                                   {project.pro_name}
                                 </Link>{" "}
@@ -124,33 +183,18 @@ function EnL_newProject({ checkPermission }) {
                       </table>
                     </div>
                   </div>
-                  <ul className="pagination float-end list">
-                    <li className="page-item disabled">
-                      <Link className="page-link" href="#" tabindex="-1">
-                        Prev
-                      </Link>
-                    </li>
-                    <li className="page-item active">
-                      <Link className="page-link" href="#">
-                        1
-                      </Link>
-                    </li>
-                    <li className="page-item">
-                      <Link className="page-link" href="#">
-                        2 <span className="sr-only">(current)</span>
-                      </Link>
-                    </li>
-                    <li className="page-item">
-                      <Link className="page-link" href="#">
-                        3
-                      </Link>
-                    </li>
-                    <li className="page-item">
-                      <Link className="page-link" href="#">
-                        Next
-                      </Link>
-                    </li>
-                  </ul>
+
+                  <div className="pagedivengl pagination-engl">
+                    <Pagination
+                        activePage={currentPage}
+                        itemsCountPerPage={itemsPerPage}
+                        totalItemsCount={data.length}
+                        pageRangeDisplayed={5}
+                        prevPageText={"prev"}
+                        nextPageText={"next"}
+                        onChange={handlePageChange}
+                      />
+                  </div>
                 </div>
               </div>
             </div>
